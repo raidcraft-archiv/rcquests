9b61336431b9b62a0fb4a84383291847a53bf555
Philip Urban <urban.philip@googlemail.com>
2013-11-19 12:47:18 +0100

    added door action

--- src/main/java/de/raidcraft/quests/actions/Block.java
+++ src/main/java/de/raidcraft/quests/actions/Block.java
@@ -5,8 +5,10 @@ import de.raidcraft.api.quests.QuestType;
 import de.raidcraft.util.ItemUtils;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.BlockState;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
+import org.bukkit.material.Door;
 
 /**
  * @author Silthus
@@ -17,8 +19,7 @@ public class Block implements QuestType {
     @Method(name = "set", type = Type.ACTION)
     public static void setBlock(Player player, ConfigurationSection data) {
 
-        Location loc =
-                new Location(player.getWorld(), data.getInt("x"), data.getInt("y"), data.getInt("z"));
+        Location loc = new Location(player.getWorld(), data.getInt("x"), data.getInt("y"), data.getInt("z"));
         String matName = data.getString("material", "AIR");
         Material material;
         material = ItemUtils.getItem(matName);
@@ -28,5 +29,22 @@ public class Block implements QuestType {
         }
 
         loc.getBlock().setType(material);
+    }
+
+    @Method(name = "door", type = Type.ACTION)
+    public static void changeDoorState(Player player, ConfigurationSection data) {
+
+        Location loc = new Location(player.getWorld(), data.getInt("x"), data.getInt("y"), data.getInt("z"));
+        boolean open = data.getBoolean("open", true);
+
+        org.bukkit.block.Block block = loc.getBlock();
+        if(block.getType() != Material.WOODEN_DOOR && block.getType() != Material.IRON_DOOR) {
+            return;
+        }
+
+        BlockState state = block.getState();
+        Door door = (Door) state.getData();
+        door.setOpen(open);
+        state.update();
     }
 }
