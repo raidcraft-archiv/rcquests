aec216fcc2a8078bd0f69450a0461767d44ddf3a
Dragonfire <bastian616-git@yahoo.de>
2014-07-17 20:46:02 +0200

    implement quest npc clicked injection

--- src/main/java/de/raidcraft/quests/npc/NPCListener.java
+++ src/main/java/de/raidcraft/quests/npc/NPCListener.java
@@ -6,6 +6,7 @@ import de.raidcraft.api.quests.InvalidQuestHostException;
 import de.raidcraft.api.quests.QuestHost;
 import de.raidcraft.api.quests.Quests;
 import de.raidcraft.quests.QuestPlugin;
+import de.raidcraft.rcconversations.api.conversation.RCConservationFindNPCConversationHost;
 import net.citizensnpcs.api.event.NPCClickEvent;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import org.bukkit.event.EventHandler;
@@ -17,40 +18,17 @@ import org.bukkit.event.Listener;
  */
 public class NPCListener implements Listener {
 
-    private QuestPlugin plugin;
-
-    public NPCListener(QuestPlugin plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler(ignoreCancelled = true)
-    public void onRightClick(NPCRightClickEvent event) {
-        ConversationHost host = getClickedHost(event);
-        if (host == null) return;
-//        plugin.get().triggerConversation(host, event.getClicker());
-    }
-
-    // TODO: check perfromance
-    // TODO: how to trigger quest actions?
-    private ConversationHost getClickedHost(NPCClickEvent event) {
+    @EventHandler
+    private void npcConservations(RCConservationFindNPCConversationHost event) {
         if (!event.getNPC().hasTrait(QuestTrait.class)) {
-            return null;
+            return;
         }
-
-        if (event.getClicker().getLocation().distance(event.getNPC().getEntity().getLocation()) > 4) {
-            return null;
-        }
-        ConversationHost host = null;
         try {
             QuestHost questHost = Quests.getQuestHost(event.getNPC().getTrait(QuestTrait.class).getHostId());
-            if (questHost != null && questHost instanceof QuestNPCHost) {
-                QuestNPCHost questNpcHost = (QuestNPCHost) questHost;
-                questNpcHost.interact(event.getClicker());
-                host = questNpcHost;
-            }
+            QuestNPCHost questNpcHost = (QuestNPCHost) questHost;
+            event.setHost(questNpcHost);
         } catch (InvalidQuestHostException e) {
             RaidCraft.LOGGER.warning(e.getMessage());
         }
-        return host;
     }
 }
