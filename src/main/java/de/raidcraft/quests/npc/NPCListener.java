f422174be036bcb434ec8e6b79a58772bc8121c6
Silthus <silthus@raid-craft.de>
2014-07-25 18:46:25 +0200

    fixed npe

--- src/main/java/de/raidcraft/quests/npc/NPCListener.java
+++ src/main/java/de/raidcraft/quests/npc/NPCListener.java
@@ -1,8 +1,6 @@
 package de.raidcraft.quests.npc;
 
-import de.raidcraft.RaidCraft;
 import de.raidcraft.api.quests.QuestHost;
-import de.raidcraft.rcconversations.conversations.ConversationManager;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -20,8 +18,6 @@ public class NPCListener implements Listener {
             QuestHost questHost = event.getNPC().getTrait(QuestTrait.class).getQuestHost();
             if (questHost != null) {
                 questHost.interact(event.getClicker());
-                // also try to trigger the default conversation if it exists
-                RaidCraft.getComponent(ConversationManager.class).triggerConversation(questHost, event.getClicker());
             }
         }
     }
