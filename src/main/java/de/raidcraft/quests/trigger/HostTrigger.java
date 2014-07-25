0b983f8c975042720493ef81bebdf8da5d6920de
Silthus <silthus@raid-craft.de>
2014-07-25 15:47:08 +0200

    added debug messages

--- src/main/java/de/raidcraft/quests/trigger/HostTrigger.java
+++ src/main/java/de/raidcraft/quests/trigger/HostTrigger.java
@@ -1,5 +1,6 @@
 package de.raidcraft.quests.trigger;
 
+import de.raidcraft.RaidCraft;
 import de.raidcraft.api.action.trigger.Trigger;
 import de.raidcraft.api.quests.QuestHostInteractEvent;
 import org.bukkit.event.EventHandler;
@@ -18,6 +19,7 @@ public class HostTrigger extends Trigger implements Listener {
     @EventHandler(ignoreCancelled = true)
     public void onQuestHostInteract(QuestHostInteractEvent event) {
 
+        RaidCraft.LOGGER.info("Triggered host.interact on " + event.getHost().getId() + " from " + event.getPlayer().getName());
         informListeners("interact", event.getPlayer(), config -> event.getHost().getId().equalsIgnoreCase(config.getString("host")));
     }
 }
