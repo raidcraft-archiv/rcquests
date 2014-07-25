8de806f0fa6e988fc5ac161a2604165283e8a654
Silthus <silthus@raid-craft.de>
2014-07-25 19:13:48 +0200

    fire the default conversation of the quest trait

--- src/main/java/de/raidcraft/quests/npc/NPCListener.java
+++ src/main/java/de/raidcraft/quests/npc/NPCListener.java
@@ -1,6 +1,8 @@
 package de.raidcraft.quests.npc;
 
+import de.raidcraft.RaidCraft;
 import de.raidcraft.api.quests.QuestHost;
+import de.raidcraft.rcconversations.conversations.ConversationManager;
 import net.citizensnpcs.api.event.NPCRightClickEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -18,7 +20,10 @@ public class NPCListener implements Listener {
         if (event.getNPC().hasTrait(QuestTrait.class)) {
             QuestHost questHost = event.getNPC().getTrait(QuestTrait.class).getQuestHost();
             if (questHost != null) {
+                questHost.setConversation(event.getClicker(), questHost.getDefaultConversationName());
                 questHost.interact(event.getClicker());
+                // fire the default conversation if set
+                RaidCraft.getComponent(ConversationManager.class).triggerConversation(questHost, event.getClicker());
             }
         }
     }
