dbc4b59916fdd014efeec06f701af50aa5bdfdb6
Dragonfire <bastian616-git@yahoo.de>
2014-08-05 19:49:27 +0200

    quest npc are only temporary fixed #1182

--- src/main/java/de/raidcraft/quests/npc/NPC_Quest_Manager.java
+++ src/main/java/de/raidcraft/quests/npc/NPC_Quest_Manager.java
@@ -25,9 +25,9 @@ public class NPC_Quest_Manager {
         return INSTANCE;
     }
 
-    public NPC createPersistNpcQuest(String name, String host, String conversationName, String hostID) {
+    public NPC createNonPersistNpcQuest(String name, String host, String conversationName, String hostID) {
 
-        NPC npc = NPC_Conservations_Manager.getInstance().createPersistNpcConservations(name, host, conversationName);
+        NPC npc = NPC_Conservations_Manager.getInstance().createNonPersistNpcConservations(name, host, conversationName);
         npc.addTrait(QuestTrait.class);
         npc.getTrait(QuestTrait.class).setHostId(hostID);
         NPC_Manager.getInstance().store(host);
@@ -36,7 +36,7 @@ public class NPC_Quest_Manager {
 
     public NPC spawnPersistNpcQuest(Location loc, String name, String host, String conversationName, String hostID) {
 
-        NPC npc = this.createPersistNpcQuest(name, host, conversationName, hostID);
+        NPC npc = this.createNonPersistNpcQuest(name, host, conversationName, hostID);
         npc.spawn(loc);
         NPC_Manager.getInstance().store(host);
         return npc;
