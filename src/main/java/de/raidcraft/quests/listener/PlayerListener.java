a5dda2be00c24c69bf5861968c77e950c18ba411
Silthus <silthus@raid-craft.de>
2015-03-28 02:08:30 +0100

    #1678 - added a message when a quest item is picked up

--- src/main/java/de/raidcraft/quests/listener/PlayerListener.java
+++ src/main/java/de/raidcraft/quests/listener/PlayerListener.java
@@ -5,6 +5,7 @@ import de.raidcraft.api.items.CustomItemStack;
 import de.raidcraft.api.items.ItemType;
 import de.raidcraft.quests.QuestPlugin;
 import de.raidcraft.quests.api.holder.QuestHolder;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -46,6 +47,9 @@ public class PlayerListener implements Listener {
             CustomItemStack customItem = RaidCraft.getCustomItem(event.getItem().getItemStack());
             if (customItem.getItem().getType() == ItemType.QUEST) {
                 plugin.getQuestManager().getQuestHolder(event.getPlayer()).getQuestInventory().addItem(customItem);
+                plugin.getTranslationProvider().msg(event.getPlayer(), "quest-item-pickup",
+                        ChatColor.AQUA, "Du hast ein Quest Item aufgenommen (%s). Öffne dein Quest Inventar mit /qi.",
+                        customItem.getItem().getName());
                 event.getItem().remove();
                 event.setCancelled(true);
             }
